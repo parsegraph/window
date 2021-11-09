504f2a5583ab587b2ddf435b80776d7d4afb1fe3
Aaron Faanes <dafrito@gmail.com>
2021-11-08 23:41:19 -0600

    Add types to BasicWindow

--- src/BasicWindow.ts
+++ src/BasicWindow.ts
@@ -4,20 +4,20 @@ import Component from "./Component";
 
 export default abstract class BasicWindow extends BasicGLProvider {
   abstract containerFor(comp:Component):Element;
-  abstract numComponents();
-  abstract isOffscreen();
+  abstract numComponents():number;
+  abstract isOffscreen():boolean;
   abstract layout(target:Component):Rect;
-  abstract forEach(func:Function, funcThisArg?:any);
-  abstract scheduleUpdate();
+  abstract forEach(func:Function, funcThisArg?:any):void;
+  abstract scheduleUpdate():void;
 
   abstract textureSize():number;
-  abstract overlay();
-  abstract overlayCanvas();
-  abstract addComponent(comp:Component);
-  abstract removeComponent(compToRemove:Component);
+  abstract overlay():CanvasRenderingContext2D;
+  abstract overlayCanvas():HTMLCanvasElement;
+  abstract addComponent(comp:Component):void;
+  abstract removeComponent(compToRemove:Component):void;
   abstract tick(startTime:number):boolean;
   abstract paint(timeout:number):boolean;
-  abstract log(msg:any);
+  abstract log(msg:any):void;
   abstract clearLog():void;
-  abstract finalizeLog();
+  abstract finalizeLog():void;
 }
