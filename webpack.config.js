8e4d66c7e4cb8260131d6868b9cad46984baead4
Aaron Faanes <dafrito@gmail.com>
2022-02-27 15:59:54 -0600

    Fix webpack rename

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const {webpackConfig, relDir} = require("./webpack.common");
 
 module.exports = {
   entry: {
-    main: relDir("src/window.ts"),
+    index: relDir("src/index.ts"),
     demo: relDir("src/demo.ts"),
   },
   ...webpackConfig(false),
