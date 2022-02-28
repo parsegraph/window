ed653dee70d7b5d5332f1cb4412e7a8279cb6901
Aaron Faanes <dafrito@gmail.com>
2022-02-27 19:40:13 -0600

    Fix tests

--- test/test.ts
+++ test/test.ts
@@ -1,78 +1,114 @@
 const assert = require("assert");
-import Window, { ProxyComponent } from "../src/index";
+import { BasicProjector, Projector, Projected } from 'parsegraph-projector';
+import GraphicsWindow from "../src/index";
+import Method from "parsegraph-method";
+
+const isParentOf = (parent:Element, elem:Element) => {
+  return elem.parentNode === parent || elem.parentElement === parent;
+};
+
+const isAncestorOf = (parent:Element, elem:Element) => {
+  // console.log("Checking if ", parent, "is ancestor of ", elem);
+  if (!parent || !elem) {
+    return false;
+  }
+  return (
+    isParentOf(parent, elem) ||
+    isAncestorOf(parent, elem.parentElement) ||
+    isAncestorOf(parent, elem.parentNode as Element)
+  );
+};
+
+class ProxyComponent implements Projected {
+  _color: string;
+  _onScheduleUpdate: Method;
+  _elems:Map<Projector, HTMLElement>;
+
+  constructor(color: string = "") {
+    this._color = color;
+    this._onScheduleUpdate = new Method();
+    this._elems = new Map();
+  }
+
+  tick() {
+    return false;
+  }
+
+  dispose() {
+  }
+
+  unmount() {
+  }
+
+  setOnScheduleUpdate(func: Function, funcObj: any) {
+    this._onScheduleUpdate.set(func, funcObj);
+  }
+
+  getElem(projector: Projector) {
+    return this._elems.get(projector);
+  }
+
+  paint(projector: Projector) {
+    if (!this._elems.has(projector)) {
+      this._elems.set(projector, document.createElement("div"));
+    }
+    return false;
+  }
+
+  render(projector: Projector) {
+    const elem = this._elems.get(projector);
+    if (!elem) {
+      return true;
+    }
+    elem.innerHTML = "Rendered";
+    return false;
+  }
+}
 
 describe("Window", function () {
   it("can be constructed", () => {
-    assert.ok(new Window());
+    assert.ok(new GraphicsWindow());
   });
 
   it("can add and remove components", () => {
-    const win = new Window();
+    const win = new GraphicsWindow();
     const comp = new ProxyComponent();
-    win.addComponent(comp);
+    win.addHorizontal(comp);
     assert.ok(win.removeComponent(comp));
     assert.ok(!win.removeComponent(comp));
   });
 
   it("has an element container for a component", () => {
-    const win = new Window();
+    const win = new GraphicsWindow();
     const comp = new ProxyComponent();
-    win.addComponent(comp);
-    const container = win.containerFor(comp);
+    win.addVertical(comp);
+    const proj = new BasicProjector();
+    win.paint(proj, 0);
+    const container = win.projectionFor(proj, comp);
     assert.ok(container);
-    const d = document.createElement("div");
-    container.appendChild(d);
-    assert.ok(container === win.containerFor(comp));
+    assert.ok(container === win.projectionFor(proj, comp));
   });
 
   it("actually adds an element to the window's container", () => {
-    const win = new Window();
+    const win = new GraphicsWindow();
     const comp = new ProxyComponent();
-    win.addComponent(comp);
-    const container = win.containerFor(comp);
-    const d = document.createElement("div");
-    container.appendChild(d);
-
-    const isParentOf = (parent, elem) => {
-      return elem.parentNode === parent || elem.parentElement === parent;
-    };
-    const isAncestorOf = (parent, elem) => {
-      // console.log("Checking if ", parent, "is ancestor of ", elem);
-      if (!parent || !elem) {
-        return false;
-      }
-      return (
-        isParentOf(parent, elem) ||
-        isAncestorOf(parent, elem.parentElement) ||
-        isAncestorOf(parent, elem.parentNode)
-      );
-    };
-    assert.ok(isAncestorOf(win.container(), d));
+    win.addHorizontal(comp);
+    const proj = new BasicProjector();
+    win.paint(proj, 0);
+    assert.ok(win.projectionFor(proj, comp));
+    assert.ok(isAncestorOf(proj.container(), comp.getElem(proj)));
   });
 
   it("removes an element container from a component", () => {
-    const win = new Window();
+    const win = new GraphicsWindow();
     const comp = new ProxyComponent();
-    win.addComponent(comp);
-    const container = win.containerFor(comp);
-    const d = document.createElement("div");
-    container.appendChild(d);
-
-    const isParentOf = (parent, elem) => {
-      return elem.parentNode === parent || elem.parentElement === parent;
-    };
-    const isAncestorOf = (parent, elem) => {
-      if (!parent || !elem) {
-        return false;
-      }
-      return (
-        isParentOf(parent, elem) ||
-        isAncestorOf(parent, elem.parentElement) ||
-        isAncestorOf(parent, elem.parentNode)
-      );
-    };
-    assert.ok(isAncestorOf(win.container(), d));
+    win.addHorizontal(comp);
+    const proj = new BasicProjector();
+    win.paint(proj, 0);
+    assert.ok(win.projectionFor(proj, comp));
+    const elem = comp.getElem(proj);
+    assert.ok(isAncestorOf(proj.container(), elem));
     assert.ok(win.removeComponent(comp));
-    assert.ok(!isAncestorOf(win.container(), d));
+    assert.ok(!isAncestorOf(proj.container(), elem));
   });
 });
